ee052b406431618246fb33129d9280fb0b5242b3
lifelebisi <life.lebisi20@gmail.com>
2024-06-04 11:06:43 +0200

    added codes

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 let number= Number (prompt('provide a number of pencils'));
 let price= Number(prompt('provide price of one pencil'));
 let amount = Number (prompt('amount due'));
+
+if (numberofpencils >=25) {
+    console.log('You qualify for discount');
+
+} 
+
+
+else if (numberofpencils<25) { 
+    console.log('you do not qualify for discount');
+} else {
+    console.log('amount= amount - (amount *0,075) ');
+}
+
