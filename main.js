1126397f3d2f80aea197ca89586a5475b35ac877
GuguM22 <gugu.mkhwanazi40@gmail.com>
2024-06-04 10:10:15 +0200

    edited file name

---
+++ main.js
@@ -0,0 +1,5 @@
+let Fname = prompt('Enter your Name');
+let age = Number(prompt('Enter your Age'));
+let citizenship = prompt('Are you South African ?');
+let greeting = 'Hi ';
+console.log(greeting + Fname);
